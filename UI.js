cf6df2aade9b83acf0d0e623fc8597f1c5991aca
xlong <xlong1060@gmail.com>
2023-11-18 16:23:52 +0800

    Update UI.js

--- UI.js
+++ UI.js
@@ -128,9 +128,10 @@ activity.setSupportActionBar(ui.toolbar);
 
 function getScript(choice) {
     let url_prefix = [
-        'https://ghproxy.com/https://raw.githubusercontent.com/lkamh/jszs/main/',
-        'https://cdn.jsdelivr.net/gh/lkamh/jszs@main/',
-        'https://raw.githubusercontent.com/lkamh/jszs/main/',
+        'https://mirror.ghproxy.com/https://raw.githubusercontent.com/lkamh/jszs/main/',
+        'https://raw.fastgit.org/lkamh/jszs/main/',
+        'https://gh-proxy.com/https://raw.githubusercontent.com/lkamh/jszs/main/',
+        'https://gh.api.99988866.xyz/https://raw.githubusercontent.com/lkamh/jszs/main/'
     ];
     for (var i = 0; i < url_prefix.length; i++) {
         try {
